93b49701607bd271b63ff2ec0339d6e7b14a6c66
Michele Comolli <mikicomolli@gmail.com>
2020-10-24 18:03:30 +0200

    Added js function

--- app/javascript/channels/index.js
+++ app/javascript/channels/index.js
@@ -3,3 +3,14 @@
 
 const channels = require.context('.', true, /_channel\.js$/)
 channels.keys().forEach(channels)
+
+// highlightFor('','lightblue', 3);
+
+function highlightFor(id,color,seconds){
+    var element = document.getElementById(id)
+    var origcolor = element.style.backgroundColor
+    element.style.backgroundColor = color;
+    var t = setTimeout(function(){
+       element.style.backgroundColor = origcolor;
+    },(seconds*1000));
+}
